9a432846c6db4f39c56a5dc2dac82b9fc996f8a4
Basyo-Engineer <svarval.lite@gmail.com>
2024-03-05 12:31:33 +0900

    Add testFill

---
+++ projects/04/fill/testFill.asm
@@ -0,0 +1,42 @@
+(LOOP)
+    // D = condition
+    //Memory Counter
+    @MEM
+    M=0
+    (BLACK)
+        @MEM
+        D=M
+        @8191
+        D=D-A
+        @MEMRST
+        A;JEQ
+
+        //SCREEN MEM fill Black
+        @SCREEN
+        D=A
+        @MEM
+        D=D+A
+        @R0
+        M=D
+        @65535
+        D=A
+        @R0
+        A=M
+        M=D
+        @MEM
+        M=M+1
+        @BLACK
+        A;JMP
+
+    (BLACK_END)
+    (MEMRST)
+        @0
+        D=A
+        @MEM
+        M=D
+        @LOOP
+        A;JMP
+    (MEMRST_END)
+    @LOOP
+    0;JMP
+(LOOP_END)
